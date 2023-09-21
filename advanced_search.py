cfe4589322540f2ebb9fc6329efa7abb7ac877d8
SatoshiGuacamole <103341519+SatoshiGuacamole@users.noreply.github.com>
2023-09-21 05:25:28 +0200

    alpha7

--- advanced_search.py
+++ advanced_search.py
@@ -1,3 +1,4 @@
+from io import StringIO
 from jinja2 import nodes
 from jinja2.ext import Extension
 from lxml import etree
@@ -24,29 +25,28 @@ class AdvancedFiltersProcessor(Extension):
     def __init__(self, environment):
         super().__init__(environment)
 
-    def find_filter_elements(self, tree):
-        return tree.find(
-            ".//input[starts-with(@name, 'category_') or @name='language' or @name='time_range' or @name='safesearch']"
-        )
-
     def preprocess(self, source, name, filename=None):
         try:
-            tree = etree.fromstring(source)
-            form = tree.find("/form[@id='search']")
+            tree = etree.parse(StringIO(source), etree.XMLParser(encoding='ISO-8859-1', ns_clean=True, recover=True))
 
-            if form is None:
-                raise Exception()
+            if etree.tostring(tree).decode('ISO-8859-1') != source:
+                if name == 'simple/simple_search.html':
+                    print("AdvancedFiltersProcessor error", source, etree.tostring(tree).decode('ISO-8859-1'))
+                return source
         except Exception as e:
-            if name == 'simple/simple_search.html':
-                logger.debug("AdvancedFiltersProcessor", source, e)
             return source
 
-        logger.debug("AdvancedFiltersProcessor", form)
+        form = tree.find("/form[@id='search']")
+
+        if form is None:
+            return source
+
+        print("AdvancedFiltersProcessor form", form)
 
         for filter_element in self.find_filter_elements(form):
             form.remove(filter_element)
 
-        return etree.tostring(tree)
+        return etree.tostring(tree).decode('ISO-8859-1')
 
     def parse(self, parser):
         if parser.stream.current.value != 'simple_search_filters':
@@ -59,7 +59,7 @@ class AdvancedFiltersProcessor(Extension):
 
         filter_elements = etree.fromstring(
             self.find_filter_elements(tree)
-        )
+        ).decode('ISO-8859-1')
 
         simple_filters = nodes.Const(f'{filter_elements}', lineno=lineno)
 
@@ -67,33 +67,35 @@ class AdvancedFiltersProcessor(Extension):
 
         return nodes.CallBlock(simple_filters, [], [], []).set_lineno(lineno)
 
+    def find_filter_elements(self, tree):
+        return tree.find(
+            ".//input[starts-with(@name, 'category_') or @name='language' or @name='time_range' or @name='safesearch']"
+        )
+
 class AssetIncluderProcessor(Extension):
     def __init__(self, environment):
         super().__init__(environment)
         self.pwd = os.path.dirname(os.path.abspath(__file__))
 
-    def encode_asset(self, path):
-        mime, encoding = mimetypes.guess_type(path)
-
-        with open(path, 'rb') as file:
-            data = base64.b64encode(file.read()).decode('utf-8')
-
-        return f'data:{mime};base64,{data}'
-
     def preprocess(self, source, name, filename=None):
         try:
-            tree = etree.fromstring(source)
+            tree = etree.parse(StringIO(source), etree.XMLParser(encoding='ISO-8859-1', ns_clean=True, recover=True))
+
+            if etree.tostring(tree) != source:
+                if name == 'simple/base.html':
+                    print("AssetIncluderProcessor error", source, etree.tostring(tree).decode('ISO-8859-1'))
+                return source
         except Exception as e:
-            if name == 'simple/base.html':
-                logger.debug("AssetIncluderProcessor", source, e)
             return source
 
         body = tree.find("//body")
         head = tree.find("//head")
 
-        logger.debug("AssetIncluderProcessor", tree, body, head)
+        print("AssetIncluderProcessor", body, head)
 
         if head:
+            print("AssetIncluderProcessor head", head)
+
             head_string = etree.tostring(head)
             link_tags = ''
 
@@ -107,6 +109,8 @@ class AssetIncluderProcessor(Extension):
             head.getparent().replace(head, etree.fromstring(head_string))
 
         if body:
+            print("AssetIncluderProcessor body", body)
+
             script_tags = ''
 
             for root, dirs, files in os.walk(os.path.join(self.pwd, 'assets', 'js')):
@@ -116,7 +120,18 @@ class AssetIncluderProcessor(Extension):
 
             body.append(etree.fromstring(script_tags))
 
-        return etree.tostring(tree)
+        output = etree.tostring(tree).decode('ISO-8859-1')
+        print("AssetIncluderProcessor output", output)
+
+        return output
+
+    def encode_asset(self, path):
+        mime, encoding = mimetypes.guess_type(path)
+
+        with open(path, 'rb') as file:
+            data = base64.b64encode(file.read()).decode('ISO-8859-1')
+
+        return f'data:{mime};base64,{data}'
 
 class SimpleMainIdProcessor(Extension):
     def __init__(self, environment):
